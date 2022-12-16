f9a99f632d6d9daaf78823da96d3462f1f5764fa
rsah8848 <rsah07541@gmail.com>
2022-12-16 19:29:23 +0545

    Variables in details

--- HelloApp/Entrypoint.cs
+++ HelloApp/Entrypoint.cs
@@ -1,8 +1,15 @@
-﻿class Entrypoint 
+﻿class Entrypoint
 {
- 
- public static void Main1(){
-    Console.WriteLine("Good to entry the programming");
-  
- }
+
+    public static void Main()
+    {
+      var number =89;
+      if(number % 2 == 0)
+      {
+         Console.WriteLine(number + "is even");
+      }
+      else{
+         Console.WriteLine(number + "is even");
+      }
+    }
 }
--- HelloApp/Variables.cs
+++ HelloApp/Variables.cs
@@ -1,3 +1,29 @@
 
-#Top level statements, the statements where only one statements is created
-Console.WriteLine("Lets learn about the variables");
+class variable{
+   // void learnVariable(){
+    //   //Numbers:integers(without decimal), floating point,
+    //   // Integers(BYTE,Short,int,) 
+    //   // byte holds 0 t0 255 like storing ages  of people
+    //   byte a = 12;
+    //   short b = 2323;
+    //   int c = 4242424;
+    //   long d = 324637345467;
+    // //floating points(long,float,double,decimal)
+    //   float e = 345.34454465656f;// float store upto 6 digits after the decimal points
+    //   double u = 355345.455;
+    //   decimal g = 354564545646.35454654566m;//most precise 
+
+    //   //text
+    //   string name = "Rahul Sah";
+    //   char gender = 'M';
+
+    //   //Boolean
+    //   bool RUMale = true;
+
+    //   //Dates and time
+    //   DateTime DO
+
+
+
+//}
+}
