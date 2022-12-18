6ed6b9f94d74a37be8669b7dcc625d20f4eeaea2
rsah8848 <rsah07541@gmail.com>
2022-12-18 19:06:31 +0545

    Variable with Simple Interest

--- HelloApp/Entrypoint.cs
+++ HelloApp/Entrypoint.cs
@@ -3,13 +3,22 @@
 
     public static void Main()
     {
-      var number =89;
-      if(number % 2 == 0)
-      {
-         Console.WriteLine(number + "is even");
+            
+            Console.WriteLine("Enter the principle");
+            var p = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the time period");
+            var t = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the rate of interest");
+            var r = float.Parse(Console.ReadLine());
+            var i = p * t * r / 100;
+            Console.WriteLine(" The required SimpleInterest is" + i);
+            var a = p + i;
+            Console.WriteLine("The required amount is" +a);
+            Console.ReadLine();
+
       }
-      else{
-         Console.WriteLine(number + "is even");
-      }
+   
     }
-}
+
+    
+
