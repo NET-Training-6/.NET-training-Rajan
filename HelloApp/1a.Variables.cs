067d9425f3e106fd36045931f51610e157a00b9c
rsah8848 <rsah07541@gmail.com>
2022-12-22 19:08:25 +0545

    Class and object

---
+++ HelloApp/1a.Variables.cs
@@ -0,0 +1,73 @@
+
+class variable{
+   // void learnVariable(){
+    //   //Numbers:integers(without decimal), floating point,
+    //   // Integers(BYTE,Short,int,) 
+    //   // byte holds 0 t0 255 like storing ages  of people
+    //   byte a = 12;
+    //   short b = 2323;
+    //   int c = 4242424;
+    //   long d = 324637345467;
+    // //floating points(long,float,double,decimal)
+    //   float e = 345.34454465656f;// float store upto 6 digits after the decimal points
+    //   double u = 355345.455;
+    //   decimal g = 354564545646.35454654566m;//most precise 
+
+    //   //text
+    //   string name = "Rahul Sah";
+    //   char gender = 'M';
+
+    //   //Boolean
+    //   bool RUMale = true;
+
+    //   //Dates and time
+    //   DateTime DO
+
+    //  Console.WriteLine("BMI Calculator");
+        //  Console.WriteLine("Enter the height in metrer");
+        //  var height = float.Parse(Console.ReadLine());
+        //  Console.WriteLine("Enter the weight in kg");
+        //  var weight = float.Parse(Console.ReadLine());
+        //  var bmi = weight / (height*height);
+        //  Console.WriteLine("The required BMI of the value is " + bmi );
+        //  Console.ReadLine();
+
+        // Console.WriteLine("Enter a number");
+        // var number = int.Parse(Console.ReadLine());
+        // if (number % 3 == 0 && number % 5 == 0)
+        // {
+        //     Console.WriteLine(number + "is multiple of 3 and 5");
+        // }
+        // else if (number % 5 == 0)
+        // {
+        //     Console.WriteLine("Only multiple of 5");
+        // }
+        // else if (number % 3 == 0)
+        // {
+        //     Console.WriteLine("Only multiple of 3");
+        // }
+        // else
+        // {
+        //     Console.WriteLine(number + " is not divisible by 3 or 5");
+        //}
+
+        // Console.WriteLine("Principle");
+        // var p = double.Parse(Console.ReadLine());
+
+        // Console.WriteLine("Time(Years)");
+        // var t = double.Parse(Console.ReadLine());
+
+        // Console.WriteLine("rate(%)");
+        // var r = double.Parse(Console.ReadLine());
+
+        // var ci = p * Math.Pow((1+r/100),t) - p;
+
+        // //Console.WriteLine("Required compound interest of" +p + "for" +t + "years, at" +rText + "% is" +ci);
+              
+        // //String Manipulation
+        // Console.WriteLine($"Required compound interest of {p} for {t} years at {r}% is {ci}");
+        
+
+
+//}
+}
--- HelloApp/Entrypoint.cs
+++ HelloApp/Entrypoint.cs
@@ -3,53 +3,32 @@
 
     public static void Main()
     {
-
-        //  Console.WriteLine("BMI Calculator");
-        //  Console.WriteLine("Enter the height in metrer");
-        //  var height = float.Parse(Console.ReadLine());
-        //  Console.WriteLine("Enter the weight in kg");
-        //  var weight = float.Parse(Console.ReadLine());
-        //  var bmi = weight / (height*height);
-        //  Console.WriteLine("The required BMI of the value is " + bmi );
-        //  Console.ReadLine();
-
-        // Console.WriteLine("Enter a number");
-        // var number = int.Parse(Console.ReadLine());
-        // if (number % 3 == 0 && number % 5 == 0)
-        // {
-        //     Console.WriteLine(number + "is multiple of 3 and 5");
-        // }
-        // else if (number % 5 == 0)
-        // {
-        //     Console.WriteLine("Only multiple of 5");
-        // }
-        // else if (number % 3 == 0)
-        // {
-        //     Console.WriteLine("Only multiple of 3");
-        // }
-        // else
-        // {
-        //     Console.WriteLine(number + " is not divisible by 3 or 5");
-        //}
-
-        Console.WriteLine("Principle");
-        var p = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("Time(Years)");
-        var t = double.Parse(Console.ReadLine());
-
-        Console.WriteLine("rate(%)");
-        var r = double.Parse(Console.ReadLine());
-
-        var ci = p * Math.Pow((1+r/100),t) - p;
-
-        //Console.WriteLine("Required compound interest of" +p + "for" +t + "years, at" +rText + "% is" +ci);
-              
-        //String Manipulation
-        Console.WriteLine($"Required compound interest of {p} for {t} years at {r}% is {ci}");
-
-        
-
+    //    Person person1 = new();
+    //    person1.name = "Bhojraj";
+    //    person1.age = 23;
+       
+    //    Person person2 = new();
+    //    Person person3 = new();
+    //    Person person4 = new();
+    //    Person person5 = new();
+    //    Person person6 = new();
+    //    Person person7 = new();
+    //    Person person8 = new();
+   
+       LandAnimal animal1 = new();
+       animal1.name = "Tommy";
+       animal1.breed = "Male";
+       animal1.age = 4;
+       animal1.color= "Black";
+       animal1.foot = 4;
+            
+       //Person person2 = new();
+    //    Person person3 = new();
+    //    Person person4 = new();
+    //    Person person5 = new();
+    //    Person person6 = new();
+    //    Person person7 = new();
+    //    Person person8 = new();
 
     }
 
