0071377e86687ef2e4104d4c891b880c8f081cd5
rsah8848 <rsah07541@gmail.com>
2022-12-25 19:20:44 +0545

    =Array

---
+++ HelloApp/1b.Classes and Object.cs
@@ -0,0 +1,32 @@
+// class Person
+// {
+//  //Members(methods{functionality},fields{}) are bydefault private and to make public we take help of access modifier.
+//  //Access modifier(pulic,private,internal,protected)
+//  public string name;
+//  public int age;
+//  string address;
+//  float height;
+//  DateTime dob;
+
+//  void CalculateBMI()
+//  {
+  
+//  }
+// }
+
+class LandAnimal //Pascal casing=class,methood // Camel Casing = field
+{
+    public string name;
+    public string breed;
+    public string color;
+    public int age;
+    public int foot;
+    public Boolean isWild;
+
+   public void Move()
+    {
+
+    }
+
+
+}
--- HelloApp/Entrypoint.cs
+++ HelloApp/Entrypoint.cs
@@ -7,28 +7,13 @@
     //    person1.name = "Bhojraj";
     //    person1.age = 23;
        
-    //    Person person2 = new();
-    //    Person person3 = new();
-    //    Person person4 = new();
-    //    Person person5 = new();
-    //    Person person6 = new();
-    //    Person person7 = new();
-    //    Person person8 = new();
-   
        LandAnimal animal1 = new();
        animal1.name = "Tommy";
        animal1.breed = "Male";
        animal1.age = 4;
        animal1.color= "Black";
        animal1.foot = 4;
-            
-       //Person person2 = new();
-    //    Person person3 = new();
-    //    Person person4 = new();
-    //    Person person5 = new();
-    //    Person person6 = new();
-    //    Person person7 = new();
-    //    Person person8 = new();
+       animal1.isWild = true;
 
     }
 
