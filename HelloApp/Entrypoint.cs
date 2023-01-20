d644a0bbe133eb602e526d36aaa54567de9451da
rsah8848 <rsah07541@gmail.com>
2023-01-20 19:06:38 +0545

    LinqAssignment

--- HelloApp/Entrypoint.cs
+++ HelloApp/Entrypoint.cs
@@ -3,11 +3,8 @@ class Entrypoint
 {
     static void Main()
     {
-        
-
-        Paragraph1 para = new();
-        para.DisplayParagraphInfo();
-
+        FileAndDirectory l = new();
+        l.LearnFiles();
 
     }
 }
